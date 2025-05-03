67ae648db303a44fd736aa14d0a5777782ff255d
Olubukola-Martins <19bukola97@gmail.com>
2025-05-03 09:01:25 +0100

    comment fetchDaily hook

--- screens/hooks/fetchDailyInfo.ts
+++ screens/hooks/fetchDailyInfo.ts
@@ -1,39 +1,9 @@
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-// const fetchDailyInfo = async (
-//   prompt = 'Fetch information from the past 12 hours on expos, grants, loans, and business news in Nigeria.'
-// ) => {
-//   try {
-//     const response = await fetch("https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct", {
-//       headers: {
-//         Authorization: `Bearer SECRET_KEY`,
-//         "Content-Type": "application/json",
-//       },
-//       method: "POST",
-//       body: JSON.stringify({ inputs: prompt }),
-//     });
-
-//     const result = await response.json();
-
-//     // Some models return an array of generated text responses
-//     const generatedText = result?.[0]?.generated_text || JSON.stringify(result);
-
-//     await AsyncStorage.setItem('dailyInfo', JSON.stringify(generatedText));
-//     console.log("fetch daily res", result);
-//     console.log("fetch daily data", generatedText);
-//     return generatedText;
-//   } catch (error) {
-//     console.error('Error fetching data:', error);
-//   }
-// };
-
-// export default fetchDailyInfo;
 
 import { View, Text } from "react-native";
 import React from "react";
 
 const fetchDailyInfo = () => {
-    
+
   return 
 };
 
